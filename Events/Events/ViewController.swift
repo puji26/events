5e6cae52d03eb42d904d06042c9cc958d0e811cf
Puji Wahono <pwahono26@gmail.com>
2020-09-04 11:39:10 +0700

    create project and structure core data

---
+++ Events/Events/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Events
+//
+//  Created by Puji Wahono on 04/09/20.
+//  Copyright © 2020 Puji Wahono. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
